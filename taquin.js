d080dfa61a96faa5eb8995f432880fa357a89916
savper <35189240+savper@users.noreply.github.com>
2018-04-16 15:18:42 +0200

    Add files via upload

---
+++ taquin.js
@@ -0,0 +1,11 @@
+
+let hole;         // l'image qui contient la case vide
+let hole_row ;    // la ligne où se trouve le trou
+let hole_column;  // la colonne où se trouve le trou
+let nbClick = 0;  // le nombre de chiffres déplacés
+
+// Traite le click sur l'image 'image' de
+// coordonnées ('ligne','colonne') dans la grille
+function clickAt(ligne,colonne,image) {
+
+}
