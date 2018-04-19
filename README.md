265f526cd5a3d0db589e134980418a3034b0684a
Félix <felix.fonteneau@etu.unice.fr>
2018-04-19 20:27:08 +0200

    zefzef

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # taquin
+
+liste de question pour le prof !  
+- Pouvons mettre taquin.html si on include du css ?
--- taquin.php
+++ taquin.php
@@ -1,5 +1,16 @@
 <?php
-  //génération de la grille 
+  //récupération de la dimension du jeu
+  $grid = $_GET["jeu"];
+
+  //si le paramètre passé n'est pas correct, on redirige vers l'index
+  if(!(isset($_GET["jeu"]) && preg_match('/^[2-3]x[2-3]$/',$grid))){
+    header('Location: index.html');
+    exit;
+  }
+
+
+  $array = glob("image/$grid/part-*.jpeg");
+	shuffle($array);
 
 
 
