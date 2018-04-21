073d6c62c7f3fd4736591d1a225382a336e4e5c3
FelixFonteneau <felix.fonteneau@etu.unice.fr>
2018-04-21 15:42:39 +0200

    up master

--- taquin.js
+++ taquin.js
@@ -5,22 +5,24 @@ window.onload = function() {
   let hole_row ;    // la ligne où se trouve le trou
   let hole_column;  // la colonne où se trouve le trou
   let nbClick = 0;  // le nombre de chiffres déplacés
- 
+
+
 
   function clickAt(ligne,colonne,image) {
 
   }
 
-  function getPlace(element) {
-    for(let i = 0; i < tabImg.length ; i++){
-      if(element == tabImg[i] ) return i;
+  function getPlace(element, nbCol = nbCol) {
+    for(var i = 0; i < tabImg.length ; i++){
+      if(element == tabImg[i] ) break;
     }
+    return [i%nbCol,Math.floor(i/nbCol);]
   }
 
   function testClick(){
     let place = getPlace(this);
-    let clig =
-    let ccol =
+    let clig = place[0];
+    let ccol = place[1];
 
   }
 
@@ -28,6 +30,8 @@ window.onload = function() {
   var hole document.getElementsByName('z')[0];         // l'image qui contient la case vide
 
   let nbLig = document.getElementsByClassName('ligne').length;
+  let nbCol = tabImg.length/nbLig;
+  console.log("Nb de lignes : "+nbLig+" Nb de cols : "+nbCol);
 
   for(let i = 0; i < tabImg.length ; i++){
     tabImg[i].onclick = testClick;
